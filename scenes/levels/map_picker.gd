72f3f89559ce48b6d9869d92a3b586b4d656e9c3
OlegVegan <krutopridumal@gmail.com>
2023-12-27 21:29:13 +0400

    lol

--- scenes/levels/map_picker.gd
+++ scenes/levels/map_picker.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-@onready var m1 = preload("res://maps/map1.tscn")
-@onready var m2 = preload("res://maps/map2.tscn")
-@onready var m3 = preload("res://maps/map3.tscn")
-@onready var m4 = preload("res://maps/map4.tscn")
-@onready var m5 = preload("res://maps/map5.tscn")
-@onready var m6 = preload("res://maps/map6.tscn")
-@onready var m7 = preload("res://maps/map7.tscn")
-@onready var m8 = preload("res://maps/map8.tscn")
-@onready var m9 = preload("res://maps/map9.tscn")
-@onready var m10 = preload("res://maps/map10.tscn")
+@onready var maps = [preload("res://maps/map1.tscn"),
+preload("res://maps/map2.tscn"),
+preload("res://maps/map3.tscn"),
+preload("res://maps/map4.tscn"),
+preload("res://maps/map5.tscn"),
+preload("res://maps/map6.tscn"),
+preload("res://maps/map7.tscn"),
+preload("res://maps/map8.tscn"),
+preload("res://maps/map9.tscn"),
+preload("res://maps/map10.tscn")]
 
 var selectedMap
 var mapName
@@ -17,38 +17,9 @@ var inst = false
 
 func _ready():
 	$Timer.wait_time = randf_range(0.0, 0.2)
-	if inst:
-		$Timer.wait_time = 0
+	if inst: $Timer.wait_time = 0
 	$Timer.start()
 
-func _on_timer_timeout():	
-	if selectedMap == 1:
-		var m = m1.instantiate()
-		add_child(m)
-	elif selectedMap == 2:
-		var m = m2.instantiate()
-		add_child(m)
-	elif selectedMap == 3:
-		var m = m3.instantiate()
-		add_child(m)
-	elif selectedMap == 4:
-		var m = m4.instantiate()
-		add_child(m)
-	elif selectedMap == 5:
-		var m = m5.instantiate()
-		add_child(m)
-	elif selectedMap == 6:
-		var m = m6.instantiate()
-		add_child(m)
-	elif selectedMap == 7:
-		var m = m7.instantiate()
-		add_child(m)
-	elif selectedMap == 8:
-		var m = m8.instantiate()
-		add_child(m)
-	elif selectedMap == 9:
-		var m = m9.instantiate()
-		add_child(m)
-	elif selectedMap == 10:
-		var m = m10.instantiate()
-		add_child(m)
+func _on_timer_timeout():
+	var m = maps[selectedMap - 1].instantiate()
+	add_child(m)
